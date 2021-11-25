676c2e296dbc025436155c73f6ea5070a59b7336
crapthings <crapthings@gmail.com>
2021-11-25 09:46:31 +0800

    add a async state example

--- src/composites/home.jsx
+++ src/composites/home.jsx
@@ -1,15 +1,23 @@
+import { useEffect } from 'react'
 import stores from '../stores'
 
 import Welcome from '../components/welcome'
 
 export default () => {
   const text = stores((state) => state.text)
+  const data = stores((state) => state.data)
+  const loadData = stores((state) => state.loadData)
+
+  useEffect(() => {
+    loadData()
+  }, [])
 
   return (
     <div>
       <div className='text-lg'>Home</div>
       <div>{text}</div>
       <Welcome />
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   )
 }
--- src/stores/index.js
+++ src/stores/index.js
@@ -1,7 +1,13 @@
 import create from 'zustand'
 
 const stores = create((set, get) => ({
-  text: 'Hello Kitty'
+  text: 'Hello Kitty',
+  data: null,
+  async loadData () {
+    const resp = await fetch('./static/data.json')
+    const data = await resp.json()
+    set({ data })
+  }
 }))
 
 export default stores
