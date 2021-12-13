478f2cd5a64a5b1f4e0050f05791778641362f45
crapthings <crapthings@gmail.com>
2021-12-13 12:14:58 +0800

    upgrade tailwindcss

--- src/composites/home.jsx
+++ src/composites/home.jsx
@@ -14,9 +14,12 @@ export default () => {
 
   return (
     <div>
-      <div className='text-lg'>Home</div>
+      <div className='text-lg underline'>Home</div>
       <div>{text}</div>
       <Welcome />
+      <div className='p-8'>
+        <button className='w-28 h-8 rounded-md text-sm text-white bg-blue-500 shadow-lg shadow-blue-500/50'>Tailwind CSS 3</button>
+      </div>
       <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   )
